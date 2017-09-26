10d8bd1f4c05bac04c941748903f52afd86ad990
Morgan Atwood <atwood.morganj@gmail.com>
2017-09-26 16:48:26 -0700

    Intial commit

---
+++ app/app.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Main = require('Main');
+
+//Destructuring ES6
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+//Load Foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+//app css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  //Router expects one prop
+  //{hashHistory}, {Main}, {Weather} are JSX expressions
+  //{Weather} is index you must you IndexLink to remove auto 'active class' check Nav.jsx
+  <Router history={hashHistory}>
+     <Route path="/" component={Main}>
+     </Route>
+  </Router>,
+  document.getElementById('app')
+
+);
